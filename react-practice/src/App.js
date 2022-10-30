e51299a04b61ddd6e63bc559be3d99a308885aad
Gantavya Saraswat <gantavya99@gmail.com>
2022-10-30 16:38:10 +0530

    Fetched data from json placeholder

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react';
 import './App.css';
-
+import axios from "axios";
+import { useEffect } from 'react';
 function App() {
+
+//Method1: Using promises
+const fetchData= ()=>{
+  axios.get("https://jsonplaceholder.typicode.com/posts").then((response)=>console.log(response.data));
+}
+
+
   return (
     <div className="App">
-     
+      <button className="btn" onClick={fetchData}> Fetch Data</button>
     </div>
   );
 }
--- react-practice/src/header.js
+++ react-practice/src/header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios.jsx';
 
-const header = () => {
+const Header = () => {
+const name='John';
+const url=`https://api.agify.io?name=${name}`;
+fetch(url).then((response)=>console.log(response));
+const weather=30;
   return (
-    <div>The weather of Mumbai is : </div>
+    <div className="header">The weather of Mumbai is : {weather} </div>
   )
 }
 
-export default header
+export default Header
